78f6c415a25453844c5d9c42fa5b442b6c739ef0
bin-xie <643693501@qq.com>
2020-11-19 09:30:18 +0800

    作业

--- day03/Test04.java
+++ day03/Test04.java
@@ -18,13 +18,13 @@ public class Test04 {
 
 
 
-        int shirtPrice = 245; //Ｔ恤价格
-        int shoePrice = 570;  //网球鞋价格
-        int padPrice = 320;   //网球拍价格
+        int shirtPrice = 245;   //Ｔ恤价格
+        int shoePrice = 570;    //网球鞋价格
+        int padPrice = 320;     //网球拍价格
         int shirtNo = 2;        //Ｔ恤件数
-        int shoeNo = 1;        //运动鞋数目
-        int padNo = 1;        //网球拍数目
-        double discount = 0.8;
+        int shoeNo = 1;         //运动鞋数目
+        int padNo = 1;          //网球拍数目
+        double discount = 0.8;  //折扣
 
         /*计算消费总金额*/
         double finalPay = (shirtPrice * shirtNo + shoePrice * shoeNo + padPrice * padNo) * discount;
