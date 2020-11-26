6004e05424dd3d8ee27a68503b986c517128ef06
bin-xie <643693501@qq.com>
2020-11-26 21:52:54 +0800

    作业

--- day07/Test01.java
+++ day07/Test01.java
@@ -5,6 +5,36 @@ public class Test01 {
      * 计算100以内的偶数之和
      */
     public static void main(String[] args) {
+        int i=1;  //初始化语句
+        int sum=0;
+        while (i<=100){     //条件判断
+            if (i % 2 == 0){    //判断是否是偶数
+                sum=sum+i;      //求和
+            }
+            i++;    //迭代部分
+        }
+        System.out.println(sum);
+
+
+        do {
+            if (i % 2 == 0){
+                sum+=i;
+            }
+            i++;
+        }while (i<=100);
+        System.out.println(sum);
+
+ //==============================================
+
+        /*int i=2;  //初始化语句   //int i=1,求的就是奇数和
+        int sum=0;
+        while (i<=100){     //条件判断
+            if (i % 2 == 0){    //判断是否是偶数
+                sum=sum+i;      //求和
+            }
+            i+=2;    //迭代部分
+        }
+        System.out.println(sum);*/
 
     }
 }
--- day07/Test02.java
+++ day07/Test02.java
@@ -5,7 +5,15 @@ public class Test02 {
      * 2012年培养学员25万人，每年增长25%。请问按此增长速度，到哪一年培训学员人数将达到100万人？
      */
     public static void main(String[] args) {
+        int year=2012;     //年
+        double students=250000;     //2012年学员数量
+        //2013年学员数量=250000*(1+0.25)
+        int i=1;
+        while (students<=1000000){
+            year++;
+            students *= 1.25;     //每年学院增长0.25
 
-
+        }
+        System.out.println("在"+year+"年时,培训学员人数将达到"+(int)(students)+"万人");
     }
 }
